dc27333d87d1a84da16fb37c8d5b10d2b2e19500
Mayank Saxena <mayanksaxena2214@gmail.com>
2024-11-04 15:20:17 +0530

    Edited Animations

--- src/components/ExpertCall/ExpertCall.jsx
+++ src/components/ExpertCall/ExpertCall.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import "./ExpertCall.css"
+import { Slide } from 'react-awesome-reveal'
 
 const ExpertCall = () => {
   return (
     <div className="expertcall-container">
+        
         <div className="heading">
-        See what we can do for you firm
+        <Slide>See what we can do for you firm</Slide>
         </div>
+        
         <button>Schedule a Expert Call</button>
     </div>
   )
